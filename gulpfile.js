8b1764cbdff49ff1ad6ca49e629f59c24458b8a6
pikapikamart <itsmelight1@gmail.com>
2021-09-19 06:36:09 +0800

    change gulpfile.js and intall dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,42 @@
 const {src, dest, watch, series} = require("gulp");
 const sass = require("gulp-dart-sass");
 const postcss = require("gulp-postcss");
+const browserSync = require("browser-sync");
 const autoPrefixer = require("autoprefixer");
 
-
 function scssTask() {
-  return src("src/Sass/style.scss", {sourcemaps: true})
-  .pipe(sass())
-  .pipe(postcss([autoPrefixer()]))
-  .pipe(dest("src/css", {sourcemaps: "."}));
+    return src("app/scss/style.scss", {sourcemaps: true})
+    .pipe(sass())
+    .pipe(postcss([autoPrefixer()]))
+    .pipe(dest("dist", {sourcemaps: "."}));
 }
 
 
+// Task for the browserSync
+function browserSyncServe(callback) {
+    browserSync.init({
+        server: {
+            baseDir: "."
+        }
+    });
+    callback();
+}
+
+// Task for the browerSync to reload
+function browserSyncReload(callback) {
+    browserSync.reload();
+    callback();
+}
+
+// Task for watching all
 function watchTask() {
-  watch(["src/Sass/**/*.scss"], scssTask);
+    watch(["*.html", "app/js/**/*.js"], browserSyncReload);
+    watch(["app/scss/**/*.scss"], series(scssTask, browserSyncReload));
 }
 
-
+// Default Gulp to run in CLI
 exports.default = series(
-  scssTask,
-  watchTask
+    scssTask,
+    browserSyncServe,
+    watchTask
 );
